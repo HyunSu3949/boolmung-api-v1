8dda4a22f2ad2b01b3b29ed98ed6183bd77d88d0
hyunsu.An <112445899+HyunSu3949@users.noreply.github.com>
2024-02-08 17:16:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const port = process.env.PORT || 3000;
 
 let server;
 
-if (process.env == "procuction") {
+if (process.env.NODE_ENV === "procuction") {
   const sslOptions = {
     key: fs.readFileSync(
       path.join(
