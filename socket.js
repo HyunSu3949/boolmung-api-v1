25aab8b88fa8cbee3dd384d283f9acdb8cb14575
hyunsu.An <ahs39490215@gmail.com>
2024-02-22 20:39:09 +0900

    이벤트네임 변경

--- socket.js
+++ socket.js
@@ -1,15 +1,16 @@
 const SocketIO = require("socket.io");
 const Room = require("./models/roomModel");
-const Chat = require("./models/Chat");
+const Chat = require("./models/chatModel");
 
 const eventName = {
-  SOCKET_CONNECT: "socket/connect",
-  SOCKET_DISCONNECT: "socket/disconnect",
-  SOCKET_CHAT: "socket/sendMessage",
-  SOCKET_JOIN: "socket/join",
-  SOCKET_RECIEVE_DISCONNECT: "socket/disconnect",
-  SOCKET_RECIEVE_CHAT: "socket/chat",
-  SOCKET_RECIEVE_JOIN: "socket/join",
+  CONNECT: "socket/connect",
+  DISCONNECT: "socket/disconnect",
+  CHAT: "socket/sendMessage",
+  JOIN: "socket/join",
+  MOVE: "socket/move",
+  FULL: "socket/full",
+  NOTFOUND: "socket/notfound",
+  GET_ROOM_INFO: "socket/getRoomInfo",
 };
 
 const actionState = {};
@@ -34,14 +35,15 @@ module.exports = (server, app) => {
   chat.on("connection", (socket) => {
     console.log("chat 네임스페이스에 접속");
 
-    socket.on(eventName.SOCKET_JOIN, async (data) => {
+    socket.on(eventName.JOIN, async (data) => {
       console.log("join 이벤트 발생");
-      const { roomId, _id, name } = data;
+      const { roomId, _id, name, image } = data;
 
       socket.user = {
         _id,
         roomId,
         name,
+        image,
       };
 
       try {
@@ -52,32 +54,34 @@ module.exports = (server, app) => {
         );
 
         if (!room) {
-          socket.emit("error-notFound", "방을 찾을 수 없습니다.");
+          socket.emit(eventName.NOTFOUND, "이미 삭제된 방입니다");
           return;
         }
 
         if (room.participants.length > room.max) {
-          socket.emit("error-full", "방이 가득 찼습니다.");
+          socket.emit(eventName.FULL, "방이 가득 찼습니다");
           return;
         }
-
+        socket.emit(eventName.GET_ROOM_INFO, room);
         socket.join(roomId);
 
-        chat.to(roomId).emit(eventName.SOCKET_CHAT, {
+        chat.to(roomId).emit(eventName.CHAT, {
           type: "system",
           message: `${name} 님이 입장하셨습니다.`,
         });
+
+        chat.to(roomId).emit(eventName.JOIN, { _id, name, image });
       } catch (error) {
         console.error("방 참여 처리 중 오류 발생:", error);
         socket.emit("error-unknown", "방 참여 처리 중 오류가 발생했습니다.");
       }
     });
 
-    socket.on(eventName.SOCKET_CHAT, async (data) => {
+    socket.on(eventName.CHAT, async (data) => {
       console.log("chat 이벤트 발생");
       const { _id, roomId, name, message } = data;
 
-      chat.to(roomId).emit(eventName.SOCKET_CHAT, {
+      chat.to(roomId).emit(eventName.CHAT, {
         type: _id == socket.user._id ? "mine" : "other",
         _id,
         name,
@@ -93,15 +97,17 @@ module.exports = (server, app) => {
       newChat.save();
     });
 
-    socket.on("move", (data) => {
-      const { roomId, _id, input, position, cameraCharacterAngleY } = data;
-      actionState[socket.id] = {
+    socket.on(eventName.MOVE, (data) => {
+      const { roomId, _id, input, position, cameraCharacterAngleY, image } =
+        data;
+
+      chat.to(roomId).emit(eventName.MOVE, {
         _id,
         input,
         position,
         cameraCharacterAngleY,
-      };
-      chat.to(roomId).emit("move", actionState);
+        image,
+      });
     });
 
     socket.on("disconnect", async () => {
@@ -115,7 +121,7 @@ module.exports = (server, app) => {
           { new: true }
         );
 
-        chat.to(roomId).emit(eventName.SOCKET_CHAT, {
+        chat.to(roomId).emit(eventName.CHAT, {
           type: "system",
           message: `${name}님이 퇴장하셨습니다.`,
         });
